d9bade2d9a2b0ec4b912279c65b64d4c66163d02
cungay <ihsan_cungay@hotmail.com>
2018-11-06 19:10:28 +0300

    AppFrameworkInit

---
+++ EkipSln/AppFramework/ActionKeys.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppFramework
+{
+    public class ActionKeys
+    {
+        /*
+        public const string Grid_Grouping = "grouping";
+        public const string Grid_Indicator = "indicator";
+        public const string Grid_SummaryFooter = "summaryfooter";
+        public const string Grid_Headers = "headers";
+        public const string Grid_Bands = "bands";
+        public const string Grid_VerticalLines = "verticallines";
+        public const string Grid_HorizontalLines = "horizontallines";
+        public const string Grid_AutoWidth = "autowidth";
+        public const string Grid_Preview = "preview";
+        public const string Grid_BestFit = "bestfit";
+        public const string Grid_Collapse = "collapse";
+        public const string Grid_Expand = "expand";
+        public const string Grid_Customization = "customization";
+        */
+        public const string Export_HTML = "ExportToHTML";
+        public const string Export_XML = "ExportToXML";
+        public const string Export_XLS = "ExportToXLS";
+        public const string Export_Text = "ExportToText";
+        public const string Export_Pdf = "ExportToPdf";
+        public const string Print = "Print";
+        public const string Preview = "Preview";
+
+        public const string New = "New";
+        public const string Save = "Save";
+        public const string Refresh = "Refresh";
+        public const string Find = "Find";
+        public const string NextFile = "NextFile";
+        public const string PrevFile = "PrevFile";
+        public const string LastFile = "LastFile";
+        public const string FirstFile = "FirstFile";
+        public const string Search = "Search";
+        public const string Import = "Import";
+    }
+}
