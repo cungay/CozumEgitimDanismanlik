0daad23bb8225aa047bc57ea715e50b46b86c619
cungay <ihsan_cungay@hotmail.com>
2018-11-15 22:46:49 +0300

    ConnectionDialog

---
+++ EkipSln/Ekip.WinApp/ConnectionDialog/IDataConnectionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ekip.WinApp.ConnectionDialog
+{
+	public interface IDataConnectionConfiguration
+	{
+		string GetSelectedSource();
+		void SaveSelectedSource(string provider);
+
+		string GetSelectedProvider();
+		void SaveSelectedProvider(string provider);
+	}
+}
---
+++ EkipSln/Ekip.WinApp/ConnectionDialog/frmLogin.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Ekip.WinApp.ConnectionDialog
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.btnOk = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.txtPwd = new DevExpress.XtraEditors.TextEdit();
+            this.txtUid = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.btnServer = new DevExpress.XtraEditors.SimpleButton();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUid.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Appearance.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOk.Appearance.Options.UseFont = true;
+            this.btnOk.ImageOptions.ImageUri.Uri = "Apply;Size16x16;Office2013";
+            this.btnOk.Location = new System.Drawing.Point(176, 140);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(84, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Giriş Yap";
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            this.btnOk.KeyUp += new System.Windows.Forms.KeyEventHandler(this.On_KeyUp);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AllowFocus = false;
+            this.btnCancel.Appearance.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCancel.Appearance.Options.UseFont = true;
+            this.btnCancel.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.False;
+            this.btnCancel.ImageOptions.ImageUri.Uri = "Undo;Size16x16;GrayScaled";
+            this.btnCancel.Location = new System.Drawing.Point(268, 140);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Vazgeç";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.EditValue = "1";
+            this.txtPwd.Location = new System.Drawing.Point(95, 90);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.txtPwd.Properties.PasswordChar = '•';
+            this.txtPwd.Size = new System.Drawing.Size(210, 22);
+            this.txtPwd.TabIndex = 29;
+            // 
+            // txtUid
+            // 
+            this.txtUid.EditValue = "Admin";
+            this.txtUid.Location = new System.Drawing.Point(95, 64);
+            this.txtUid.Name = "txtUid";
+            this.txtUid.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.txtUid.Size = new System.Drawing.Size(210, 22);
+            this.txtUid.TabIndex = 28;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(61, 94);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(29, 13);
+            this.labelControl3.TabIndex = 31;
+            this.labelControl3.Text = "Şifre :";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.labelControl2.Appearance.Options.UseForeColor = true;
+            this.labelControl2.Location = new System.Drawing.Point(46, 66);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(44, 13);
+            this.labelControl2.TabIndex = 30;
+            this.labelControl2.Text = "Kullanıcı :";
+            // 
+            // btnServer
+            // 
+            this.btnServer.Appearance.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnServer.Appearance.Options.UseFont = true;
+            this.btnServer.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnServer.ImageOptions.Image")));
+            this.btnServer.Location = new System.Drawing.Point(6, 141);
+            this.btnServer.Name = "btnServer";
+            this.btnServer.Size = new System.Drawing.Size(130, 23);
+            this.btnServer.TabIndex = 2;
+            this.btnServer.Text = "&Sunucu Ayarları";
+            this.btnServer.Click += new System.EventHandler(this.btnServer_Click);
+            this.btnServer.KeyUp += new System.Windows.Forms.KeyEventHandler(this.On_KeyUp);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Image = ((System.Drawing.Image)(resources.GetObject("pbImage.Image")));
+            this.pbImage.Location = new System.Drawing.Point(311, 5);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(48, 48);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbImage.TabIndex = 5;
+            this.pbImage.TabStop = false;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.labelControl1.Location = new System.Drawing.Point(73, 37);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(220, 16);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "Lütfen kullanıcı bilgilerinizi giriniz. ";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.pbImage);
+            this.panelControl1.Controls.Add(this.labelControl1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(359, 58);
+            this.panelControl1.TabIndex = 33;
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 168);
+            this.ControlBox = false;
+            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUid);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnServer);
+            this.Controls.Add(this.btnOk);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oturum Aç";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.On_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.txtPwd.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUid.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.SimpleButton btnOk;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.TextEdit txtPwd;
+        private DevExpress.XtraEditors.TextEdit txtUid;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnServer;
+        private System.Windows.Forms.PictureBox pbImage;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+    }
+}
