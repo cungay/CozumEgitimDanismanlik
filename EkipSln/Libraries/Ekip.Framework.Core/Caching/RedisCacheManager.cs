55b9b0bc7d04b85f500519b7ab56a671aeb8ea18
ihsan.cungay <ihsan_cungay@hotmail.com>
2019-06-20 03:00:09 +0300

    RedisCacheManager

---
+++ EkipSln/Libraries/Ekip.Framework.Core/Caching/RedisCacheManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ekip.Framework.Core.Caching
+{
+    public class RedisCacheManager : ICacheManager
+    {
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSet(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, object data, int cacheTime = 60)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EkipSln/Libraries/Ekip.Framework.Core/Configuration/RedisConfigurationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace Ekip.Framework.Core.Configuration
+{
+    public class RedisConfigurationManager
+    {
+        #region Constants
+
+        private const string SectionName = "RedisConfiguration";
+
+        public static RedisConfigurationSection Config
+        {
+            get
+            {
+                return (RedisConfigurationSection)ConfigurationManager.GetSection(SectionName);
+            }
+        }
+
+        #endregion
+    }
+}
